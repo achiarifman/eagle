f5abecc82b2254723f336f6110451c8e51bc44ba
Achia Rifman <achia.rifman@vidmind.com>
2014-06-20 13:57:41 +0300

    Initial eagle project

---
+++ eagle/src/main/java/com/eagle/rest/RestApplication.java
@@ -0,0 +1,12 @@
+package com.eagle.rest;
+
+import com.sun.jersey.api.core.PackagesResourceConfig;
+
+/**
+ * Created by Achia.Rifman on 20/06/2014.
+ */
+public class RestApplication extends PackagesResourceConfig {
+    public RestApplication() {
+        super("com.eagle.rest");
+    }
+}
---
+++ eagle/src/main/java/com/eagle/rest/TranscodeRestService.java
@@ -0,0 +1,14 @@
+package com.eagle.rest;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+
+/**
+ * Created by Achia.Rifman on 20/06/2014.
+ */
+@Path("transcode")
+@Consumes("application/json")
+@Produces("application/json")
+public class TranscodeRestService {
+}
