8fc8aa7553473fd589e76cbbf1ac8567e1b5cb03
Achia Rifman <achia.rifman@vidmind.com>
2014-06-30 19:00:33 +0300

    Start modify jobs

--- eagle/src/main/java/com/eagle/rest/TranscodeRestService.java
+++ eagle/src/main/java/com/eagle/rest/TranscodeRestService.java
@@ -1,8 +1,10 @@
 package com.eagle.rest;
 
-import javax.ws.rs.Consumes;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import com.eagle.entity.Record;
+import com.eagle.mgmt.TranscodeManagerService;
+import com.sun.jersey.api.core.InjectParam;
+
+import javax.ws.rs.*;
 
 /**
  * Created by Achia.Rifman on 20/06/2014.
@@ -10,5 +12,38 @@ import javax.ws.rs.Produces;
 @Path("transcode")
 @Consumes("application/json")
 @Produces("application/json")
-public class TranscodeRestService {
+public class TranscodeRestService extends RestApplication{
+
+    @InjectParam
+    TranscodeManagerService transcodeManagerService;
+
+    @POST
+    @Path("channel")
+    public String recordChannel(Record record){
+
+            transcodeManagerService.addJob(record);
+            return "{status : \"started\"}";
+    }
+
+    @GET
+    @Path("status")
+    public String getTranscoderStatus(){
+
+        if(transcodeManagerService.isFree()){
+            return "{status : \"idle\"}";
+        }
+        else
+            return "{status : \"busy\"}";
+    }
+
+    @GET
+    @Path("cancel")
+    public String cancelCurrentTranscodingJob(){
+
+        transcodeManagerService.cancelCurrentJob();
+        return "{status : \"canceled\"}";
+
+    }
+
+
 }
